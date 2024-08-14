cf0efa2a16535339042b3b1142bd88a79e992056
bhavyansh <bhavyansh2018@gmail.com>
2024-08-14 15:43:07 +0530

    uncommenting dynamic color

--- app/src/main/java/com/example/wallet_app_ui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wallet_app_ui/ui/theme/Theme.kt
@@ -8,8 +8,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
@@ -92,14 +90,14 @@ private val darkScheme = darkColorScheme(
 fun WalletAppUITheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-//    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = true,
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
-//      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//          val context = LocalContext.current
-//          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//      }
+      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+          val context = LocalContext.current
+          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+      }
 
       darkTheme -> darkScheme
       else -> lightScheme
